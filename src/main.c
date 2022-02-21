5dd1c9adb2c03d592df486fd668e69e815b176ef
John Parker <johnparker.jjp@gmail.com>
2022-02-21 18:16:21 +0000

    Basic CLI arg parsing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include "codegen.h"
 #include "lexer.h"
 #include "parser.h"
@@ -40,31 +41,73 @@ char* read_file(const char* filename, size_t* size) {
 	return charBuffer;
 }
 
+static char* filepath = NULL;
+static char* cli_source = NULL;
+
+void usage_exit(FILE* out) {
+	fprintf(out, "Possible Arguments:\n");
+	fprintf(out, "zephyr <options> <source>\n");
+	fprintf(out, "    -c => Provide a source string directly on the CLI\n");
+	fprintf(out, "          Cannot specify with a source file\n");
+	fprintf(out, "    -h => Print this help message\n");
+
+	exit(1);
+}
+
+const char** parse_cli_args(int argc, char const* argv[]) {
+	for(int i = 0; i < argc; i++) {
+		if(strcmp(argv[i], "-c") == 0) {
+			if(argc - i == 0) usage_exit(stderr);
+			cli_source = (char*)argv[i + 1];
+			i++;
+		}
+		else if(strcmp(argv[i], "-h") == 0) {
+			usage_exit(stdout);
+		}
+		else {
+			if(filepath != NULL || cli_source != NULL)
+				usage_exit(stderr);
+			filepath = (char*)argv[i];
+		}
+	}
+	return &argv[argc];
+}
+
 int main(int argc, char const *argv[]) {
 
-	//TODO Have actual argument parsing
-	if(argc != 2) {
-		fprintf(stderr, "Usage:\n%s <filename>\n", argv[0]);
-		return 1;
+	if(argc == 1) {
+		usage_exit(stderr);
+	}
+
+	parse_cli_args(argc - 1, &argv[1]);
+
+	if(filepath != NULL && cli_source != NULL) {
+		fprintf(stderr, "Cannot specify source file and source string\n");
+		usage_exit(stderr);
 	}
 
 	size_t length;
-	char* source = read_file(argv[1], &length);
+	char* source = cli_source != NULL ? cli_source : read_file(filepath, &length);
 
-	Lexer lexer = new_lexer(argv[1], source);
+	Lexer lexer = new_lexer(filepath == NULL ? "cli" : filepath, source);
 
 	Parser parser = new_parser(&lexer);
 
 	Node* ast = parse_program(&parser);
 
-	if(!parser.error)
-		print_ast(ast);
+	if(parser.error)
+		return 1;
+
+	print_ast(ast);
 
 	FILE* out = fopen("./out.yasm", "w");
 	generate_program(ast, out);
 	fclose(out);
 
-	free(source);
+	// Source was read from a file - free it
+	// If it's from the CLI do nothing
+	if(cli_source == NULL)
+		free(source);
 
 	//TODO This isn't very nice
 	int status = system("yasm -felf64 out.yasm");
