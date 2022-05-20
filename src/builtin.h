2d1365b27ac5dbb394793c07aa50b9205f16bdda
John Parker <johnparker.jjp@gmail.com>
2022-05-20 22:26:28 +0100

    Move zpr to src and remove C source code

--- src/builtin.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "ast.h"
-#include <stdio.h>
-Node** parser_builtin_functions(int* funcCount);
-void write_builtin_functions(FILE* out);
--- src/file.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <stddef.h>
-char* read_file(const char* filename, size_t* size);
